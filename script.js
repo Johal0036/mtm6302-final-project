070cd37ebbcb435c766f0d40f45f2e0a9fd1fcb7
Sajandeep Johal <joha0036@algonquinlive.com>
2021-12-16 20:29:43 -0500

    added styles

--- script.js
+++ script.js
@@ -4,12 +4,57 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=5GG6dxfCUHvQ2nMGTX60QjmwkIszo
     })
     .then(function (json) {
         console.log(json)
-        // nasa image background
+        // NASA photo
         const photo = document.getElementById("photo")
         photo.style.backgroundImage = `url(${json.url})`
 
-        // nasa caption
+        // NASA caption
         const caption = document.getElementById("caption")
-        caption.innerHTML = `<p>Title:</p> ${json.title}`
+        caption.innerHTML = `<p>Title:</p> ${json.title}. <br/> <p>Description:</p> ${json.explanation}.`
     })
-const DateTime = luxon.DateTime;
+
+//Luxon library
+const DateTime = luxon.DateTime
+const now = DateTime.local()
+const currentDate = DateTime.fromISO()
+
+const date = document.getElementById("date")
+date.innerHTML = now.toFormat("DDDD")
+const time = document.getElementById("time")
+time.innerHTML = now.toLocaleString(DateTime.TIME_24_WITH_SHORT_OFFSET)
+
+
+const greetings = document.getElementById("greetings");
+const message = ["Hello. Good morning!", "Hi. Good afternoon!", "Good evening!"]
+
+function greeting() {
+    if (now.hour < 12) {
+        greetings.innerHTML = message[0];
+    } else if (now.hour >= 12 && now.hour <= 16) {
+        greetings.innerHTML = message[1];
+    } else if (now.hour >= 17) {
+        greetings.innerHTML = message[2];
+    }
+}
+greeting();
+
+// Buttons for additional features
+const week = document.getElementById("button1")
+const month = document.getElementById("button2")
+const year = document.getElementById("button3")
+const result = document.getElementById("result")
+
+week.addEventListener("click", function (e) {
+    e.preventDefault();
+    result.innerHTML = DateTime.local().weekdayLong;
+})
+
+month.addEventListener("click", function (e) {
+    e.preventDefault()
+    result.innerHTML = DateTime.local().day;
+})
+
+year.addEventListener("click", function (e) {
+    e.preventDefault()
+    result.innerHTML = DateTime.local().ordinal;
+})
